6daec348a92709613c4fca4e09bcead55fdb1b15
Ryan Marks <Ryan@Sigmabot.com>
2015-02-05 23:24:32 -0500

    Semimajor refactor of the Course class, added commentsand removed copypasted code

--- server/MacCourseLoader.py
+++ server/MacCourseLoader.py
@@ -10,56 +10,56 @@ class MacCourseLoader():
 	current_dept=None
 	peeked_line=False
 	line_no=0
-	
-	
+
+
 	def pop_course(self):
 		#Reads one course from file
 		#returns None if course isn't scheduled for this year
 		#Otherwise gives course
-		
+
 		line=self.read_line()
-		
+
 		#Occasionally instead of a course's information a line will have a new
 		#department, if this is the case set the department to the current line
 		#and then read in another line which should be a course code
 		if self.is_dept(line):
 			self.current_dept=line
-			line=self.read_line()	
-			
+			line=self.read_line()
+
 		course_code=line
 
 		if not self.is_course_code(course_code):
 			print(course_code)
 			print(self.line_no)
 		assert self.is_course_code(course_code)
-		
+
 		course_name=self.read_line()
-		
+
 		line=self.read_line()
 		if line in "NOT OFFERED":#This exact string is used to indicate not offered courses
 			return None
 		course_term=int(line[-1])#Get the last character of the line, "T1" -> 1
-		
+
 		course_section=self.read_line()#DAY or NIGHT are the exact strings
 		assert self.is_section(course_section)
-		
+
 		#All of the course's information has been loaded so the course is created
 		new_course=Course(self.current_dept,course_code,course_name,course_term,course_section)
-		
+
 		#If information about what site the course is for is given, it is skipped over
 		if "SITE STUDENTS" in self.peek_line():
 				self.read_line()
-				
+
 		#If the course is cancelled read ahead until the next line is a course or a department
 		if "CANCELLED" in self.peek_line():
 			while not (self.is_course_code(self.peek_line()) or self.is_dept(self.peek_line())):
 				self.read_line()
 			return None
-		
+
 		#Read in all of the courses segments
-		while (self.is_class_type(self.peek_line()) or 
-				"EOW" in self.peek_line() or 
-				"CANCELLED" in self.peek_line() or 
+		while (self.is_class_type(self.peek_line()) or
+				"EOW" in self.peek_line() or
+				"CANCELLED" in self.peek_line() or
 				"SITE" in self.peek_line()):
 
 			new_course_segment=self.read_course_segment()
@@ -67,24 +67,24 @@ class MacCourseLoader():
 				new_course.add(new_course_segment)
 			if "EOW" in self.peek_line() or "SITE" in self.peek_line():
 				self.read_line()
-		new_course.consolidate_courses()
+		#new_course.consolidate_segments()
 		return new_course
-		
+
 	def read_course_segment(self):
 		#Reads one course segment
 		#Ensures the course is running
 		#Gets the note if any
 		new_segment=CourseSegment()
 		line=self.read_line()
-		
-		
+
+
 		if "EOW" in line:
 			new_segment.eow=True
 			line=self.read_line()
-		
+
 		if "CANCELLED" in line:
 			while not (self.is_course_code(self.peek_line()) or
-					   self.is_class_type(self.peek_line()) or 
+					   self.is_class_type(self.peek_line()) or
 					   self.is_dept(self.peek_line())):
 				self.read_line()
 			return None
@@ -93,8 +93,8 @@ class MacCourseLoader():
 		new_segment.name=line
 
 		if "TBA" in self.peek_line() or "CANCELLED" in self.peek_line():
-			while not (self.is_course_code(self.peek_line()) or 
-						self.is_class_type(self.peek_line()) or 
+			while not (self.is_course_code(self.peek_line()) or
+						self.is_class_type(self.peek_line()) or
 					 	self.is_dept(self.peek_line())):
 
 				self.read_line()
@@ -104,15 +104,15 @@ class MacCourseLoader():
 			line=self.read_line()
 			assert self.is_days(line)
 			days=line.split()
-			
+
 			start_hour,start_min=self.read_time()
 			end_hour,end_min=self.read_time()
 			term=int(self.read_line())
-			
+
 			for day in days:
 				block=TimeBlock(TimeBlock.day_to_num[day],start_hour,start_min,end_hour,end_min,term)
 				new_segment.add(block)
-			
+
 			line=self.peek_line()
 			if self.is_room(line):
 				new_segment.room=self.read_line()
@@ -123,22 +123,22 @@ class MacCourseLoader():
 			if self.is_note(line):
 				new_segment.note=self.read_line()
 		return new_segment
-			
+
 	def read_time(self):
 		line=self.read_line()
 		assert self.is_time(line)
 		hour,min=line.split(":")
 		hour=int(hour)
 		min=int(min)
-		return hour,min	
-	
+		return hour,min
+
 	def make_text(self,html_file_name,output_file_name):
 		#Open HTML
 		#Scrape it with beautiful soup
 		#write out to a file
 		self.html_file_name=html_file_name
 		self.output_file_name=output_file_name
-		
+
 		with open (self.html_file_name, "r") as myfile:
 			html_doc=myfile.read()
 			soup = BeautifulSoup(html_doc)
@@ -147,9 +147,9 @@ class MacCourseLoader():
 			fo.write(stripped)
 			# Close opened file
 			fo.close()
-		
+
 		self.stripped_file=open(output_file_name, 'r')
-		
+
 	def preload(self):
 		#Try to open file
 		#if it does not exist yet
@@ -158,11 +158,11 @@ class MacCourseLoader():
 			self.stripped_file=open(self.output_file_name, 'r')
 		except:
 			self.make_text(self.html_file_name,self.output_file_name)
-            self.stripped_file=open(self.output_file_name, 'r')
+			self.stripped_file=open(self.output_file_name, 'r')
 
 		for i in range(0,2):#there are 10 lines before the first department
 			self.read_line()
-			
+
 	def peek_line(self):#Reads the the next line in the file but stores it as peekedline
 		self.peeked_line=self.read_line()
 		return self.peeked_line
@@ -173,19 +173,22 @@ class MacCourseLoader():
 			self.peeked_line=False
 		else:
 			line=self.stripped_file.readline()
-			self.line_no+=1
-            if line == "":#An empty string indicates EOF
+			self.line_no += 1
+			if line == "":
+				#An empty string indicates EOF
 				raise EOFError()
 			line=str(line).strip()#Strip whitespace from the read line
-            while line=="":#If the line was just whitespace
+
+			while line == "":
+				#If the line was just whitespace
 				self.line_no+=1
 				line=self.stripped_file.readline()
 				if line == "":#An empty string indicates EOF
 					raise EOFError()
-                
+
 				line=str(line).strip()#Strip whitespace and newlines
 		return line
-		
+
 	def is_note(self,text):
 		#list other methods
 		#iterate and try them all
@@ -200,43 +203,42 @@ class MacCourseLoader():
 			if check(text):
 				return False
 		return True
-		
+
 	def is_course_code(self,text):
 		if len(text)!=4:
 			return False
-		return (not text[0].isalpha())and(text[1].isalpha())and(not text[3].isalpha())	
-		
+		return (not text[0].isalpha())and(text[1].isalpha())and(not text[3].isalpha())
+
 	def is_class_type(self,text):
 		if not len(text)==3:
 			return False
 		return ((text[0].isalpha())and(not text[1].isalpha())and(not text[2].isalpha()))
-		
+
 	def is_room(self,text):
 		if "MHK/CAMPUS" in text or "CON/CAMPUS" in text:
 			return True
 		return ("/" in text) and len(text)<10
-		
+
 	def is_prof(self,text):
 
 		return ("," in text) and (text.count(" ")<=5)
-		
+
 	def is_time(self,text):
 		if len(text)!=5 or not(":" in text[2]):
 			return False
 		return text.replace(":","").isnumeric()
-		
+
 	def is_section(self,text):
 
-		return text == "DAY" or text == "EVE"	
-		
+		return text == "DAY" or text == "EVE"
+
 	def is_days(self,text):
 		words=text.split()
 		for word in words:
 			if not word in TimeBlock.day_to_num:
 				return False
 		return True
-		
+
 	def is_dept(self,text):
 
 		return "(" in text and ")" in text
-		
--- server/Schedule.py
+++ server/Schedule.py
@@ -7,10 +7,10 @@ from itertools import product
 from pprint import pprint
 
 class Schedule():
-	
+
 	def __init__(self,courses):
 		self.courses=courses#A dict of all the attended courses using the tuple keys as keys
-		self.attended_segments=dict()#The dict will use the courses tuple_key as the key with the value being a tuple of the attended course segments as "C01"		
+		self.attended_segments=dict()#The dict will use the courses tuple_key as the key with the value being a tuple of the attended course segments as "C01"
 		self.winning_segments=dict()#This will hold the current winner
 		self.recursion_depth=0
 		self.winning_score=-10000
@@ -40,7 +40,7 @@ class Schedule():
 				pprint(options)
 			matrix.append(options)
 			self.combinations*=sum(1 for _ in product(*matrix))
-					
+
 	def is_valid(self):
 		for course1 in self.attended_segments.keys():
 			for segment1 in self.attended_segments[course1]:
@@ -55,7 +55,7 @@ class Schedule():
 							if self.courses[course1].segments[segment1].conflict_with(self.courses[course2].segments[segment2]):
 								return False
 		return True
-		
+
 	def score(self):
 		score=0
 		for key in self.attended_segments.keys():
@@ -63,7 +63,7 @@ class Schedule():
 				for time in range(1,len(self.time_prefs[:][1])):
 					hour=int((time-1)/2)+8
 					minute=(time+1)%2*30
-					
+
 					seg=CourseSegment()
 					seg.add(TimeBlock(day+1,hour,minute,hour,minute+10,3))
 					for segment in self.attended_segments[key]:
@@ -71,22 +71,22 @@ class Schedule():
 							score+=int(self.time_prefs[day][time])
 		print(score)
 		return score
-			
+
 	def pick_courses(self):
 		self.recursion_depth+=1
 		#print("-"*self.recursion_depth)
 		pick_for=self.unpicked().pop()
 		self.pick_segments(self.courses[pick_for])
 		self.recursion_depth-=1
-	
+
 	def print_winner(self):
 		print("-*-*-Schedule-*-*-*-")
 		pprint(self.winning_segments)
 		print("-*-*-        -*-*-*-")
-	
+
 	def unpicked(self):
 		return set(set(self.courses.keys()-self.attended_segments.keys()))
-	
+
 	def conflict_with(self,tuple_key):
 		course1=tuple_key
 		for segment1 in self.attended_segments[course1]:
@@ -100,9 +100,9 @@ class Schedule():
 					for segment2 in self.attended_segments[course2]:
 						if self.courses[course1].segments[segment1].conflict_with(self.courses[course2].segments[segment2]):
 							return True
-		return False						
-				
-	def solution_found(self): 
+		return False
+
+	def solution_found(self):
 			self.tried_combinations+=1
 			if self.is_valid():
 				current_score=self.score()
@@ -110,13 +110,13 @@ class Schedule():
 					pprint(self.attended_segments)
 					self.winning_segments=dict(self.attended_segments)
 					self.winning_score=current_score
-				
+
 	def unique_first_chars(self,list):
 		chars=[]
 		for item in list:
 			chars.append(item[0])
 		return set(chars)
-			
+
 	def percent_complete(self):
 		try:
 			self.layer_progress[self.recursion_depth]+=1
@@ -127,7 +127,7 @@ class Schedule():
 		if total_progress>(self.last_percent+0.1):
 			print("{:.1f}".format(total_progress) +" % Complete")
 			self.last_percent=total_progress
-			
+
 	def pick_segments(self,course):
 		matrix=[]
 		for type in course.coincident_segments.keys():
@@ -136,12 +136,12 @@ class Schedule():
 			for segment in segments.keys():
 				options.append(segments[segment][0].name)
 			matrix.append(options)
-			
+
 		try:
 			self.layer_sizes[self.recursion_depth]
 		except KeyError:
-			self.layer_sizes[self.recursion_depth]=sum(1 for _ in product(*matrix))		
-				
+			self.layer_sizes[self.recursion_depth]=sum(1 for _ in product(*matrix))
+
 		for combo in product(*matrix):
 			self.percent_complete()
 			self.attended_segments[course.tuple_key()]=list(combo)
