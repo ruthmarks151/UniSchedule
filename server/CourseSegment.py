333937642175bbcc55e19bf429e136df1702cee0
Ryan Marks <Ryan@Sigmabot.com>
2015-02-06 18:03:13 -0500

    Added comments to the CourseSegment Class and removed commented out test code

--- server/CourseSegment.py
+++ server/CourseSegment.py
@@ -4,36 +4,38 @@ class CourseSegment:
 		self.name=""
 		self.prof=""
 		self.room=""
-		self.time_blocks = []
+		self.time_blocks = []#A list that holds every TimeBlock object for this course segment
 		self.note=""
 		self.eow=False
-		
+
 	def add (self,given_block):
 		self.time_blocks.append(given_block)
-		
+
 	def blocks(self):
 		return self.time_blocks
-	
-	def conflict_with (self,other_segment):
+
+	def conflict_with (self,other_segment):#Check if the segment conflicts with another segment
 		for block in self.time_blocks:
 			for other in other_segment.blocks():
 				if block.conflict_with(other):
 					if not (self.eow and other_segment.eow):
 						return True
 		return False
-	
+
 	def coincident_with(self,other):
 		if not len(self.time_blocks)==len(other.time_blocks):
+			#If there are different numbers of blocks, they obviously aren't coincedent
 			return False
 		for block in self.time_blocks:
 			if not block.tuple_key() in [b.tuple_key() for b in other.time_blocks]:
+				#If none of the other block's tuple key matches the current block then they aren't coincedent
 				return False
 		return True
-	
-	def tuple_key(self):
+
+	def tuple_key(self):#A tuble of all the tuple keys of the time blocks
 		return tuple(f.tuple_key() for f in self.time_blocks)
-	
-	def to_string(self):
+
+	def to_string(self):# Convert the Segment to a reasonably nice string
 		out=""
 		if self.eow:
 			out+="EOW"
@@ -43,26 +45,3 @@ class CourseSegment:
 		if self.prof:	out+=self.prof+"\n"
 		if self.note:	out+=self.note+"\n"
 		return out
-
-"""		
-chem_core_1=CourseSegment()
-chem_core_1.add(TimeBlock(1,8,00,9,00,1))
-chem_core_1.add(TimeBlock(2,8,30,9,30,1))
-
-math_core_1=CourseSegment() 
-math_core_1.add(TimeBlock(4,9,00,10,30,1))
-math_core_1.add(TimeBlock(1,9,00,10,0,1))
-
-math_core_2=CourseSegment() 
-math_core_2.add(TimeBlock(1,8,30,10,30,1))
-math_core_2.add(TimeBlock(3,9,00,10,0,1))
-
-if not chem_core_1.coincident_with(chem_core_1):
-	print("Coincidince check failed")
-
-if(chem_core_1.conflict_with(math_core_1)):
-	print("False Positive")
-	
-if not (chem_core_1.conflict_with(math_core_2)):
-	print("Missed Conflict")
-"""
